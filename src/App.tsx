e22f197416dc776f947fd5aedd0c786ac223869f
Tom <tom.avedissian@polymtl.ca>
2021-05-12 13:48:36 -0400

    Testing use effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState<string>('');
+  useEffect(() => {
+    const queryInfo = { active: true, lastFocusedWindow: true };
+    chrome.tabs &&
+      chrome.tabs.query(queryInfo, tabs => {
+        const url = tabs[0].url;
+        if(url) setUrl(url)
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React
-        </a>
+        <p>URL:</p>
+        <p>{url}</p>
       </header>
     </div>
   );
